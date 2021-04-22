ece0862a55f6b2896c97fb1587ef08bae339b4bb
7-RED <a27032522@gmail.com>
2021-04-22 11:27:26 +0800

    FirstAndTest

---
+++ MedStatsCucumbereish/MedStatsCucumbereish/MedStatsCucumbereishApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedStatsCucumbereishApp.swift
+//  MedStatsCucumbereish
+//
+//  Created by 陳其宏 on 2021/4/22.
+//
+
+import SwiftUI
+
+@main
+struct MedStatsCucumbereishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
